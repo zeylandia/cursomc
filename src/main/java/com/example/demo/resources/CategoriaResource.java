782d895d8853e3e404b761222638b23937f689b1
zeylandia <wesley.felipe12@hotmail.com>
2021-07-14 18:41:05 -0300

    testando

---
+++ src/main/java/com/example/demo/resources/CategoriaResource.java
@@ -0,0 +1,31 @@
+package com.example.demo.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.domain.Categoria;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+	
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public List<Categoria> listar() {
+		
+		
+		Categoria cat1 = new Categoria(1, "informatica");
+		Categoria cat2 = new Categoria(2, "escritorio");
+		
+		List<Categoria> Lista = new ArrayList<>();
+		Lista.add(cat1);
+		Lista.add(cat2);
+		
+		return Lista;
+	}
+
+}
